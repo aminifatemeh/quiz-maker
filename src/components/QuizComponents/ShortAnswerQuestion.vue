a3d96fc2e353feae226f37a01de45d7671ce9891
Aminifatemeh <aminifatemeh1382@gmail.com>
2025-01-05 11:03:06 +0330

    feat: create true/false and short answer question components

---
+++ src/components/QuizComponents/ShortAnswerQuestion.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container class="pa-0">
+    <v-text-field
+      label="Expected Answer"
+      v-model="questionData.correctAnswer"
+      outlined
+      class="pa-0"
+    />
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ShortAnswerQuestion",
+  props: {
+    question: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      questionData: {
+        ...this.question,
+        correctAnswer: this.question.correctAnswer,
+      },
+    };
+  },
+  methods: {
+    emitQuestion() {
+      this.$emit("update-question", this.questionData);
+    },
+  },
+  watch: {
+    question: {
+      handler(newQuestion) {
+        this.questionData = {
+          ...newQuestion,
+          correctAnswer: newQuestion.correctAnswer,
+        };
+      },
+      deep: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/QuizComponents/TrueFalseQuestion.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container class="pa-0">
+    <v-radio-group
+      v-model="questionData.correctAnswer"
+      label="Select the correct answer"
+      row
+      class="pa-0"
+    >
+      <v-radio label="True" value="True" />
+      <v-radio label="False" value="False" />
+    </v-radio-group>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "TrueFalseQuestion",
+  props: {
+    question: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      questionData: {
+        ...this.question,
+        correctAnswer: this.question.correctAnswer,
+      },
+    };
+  },
+  methods: {
+    emitQuestion() {
+      this.$emit("update-question", this.questionData);
+    },
+  },
+  watch: {
+    question: {
+      handler(newQuestion) {
+        this.questionData = {
+          ...newQuestion,
+          correctAnswer: newQuestion.correctAnswer,
+        };
+      },
+      deep: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
